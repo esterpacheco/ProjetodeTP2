26b93251d0384f6c36ea4a20b91dae9b2cbc85a7
esterpacheco <e.pacheco@gsuite.iff.edu.br>
2023-12-01 15:05:37 -0300

    sqxzws

--- TP2/index.js
+++ TP2/index.js
@@ -40,28 +40,43 @@ app.post('/login/', async (req,res)=>{
     }
     res.send(usuarioEncontrado)
 })
-
-app.post('/getProdutos/', async (req,res)=>{
-    const produtoEncontrado = await modelodeUsuario.findOne({nomedoProduto: req.body.nomedoProduto, valordoProduto: req.body.valordoProduto})
-    if(produtoEncontrado === null){
-        return res.send('Produto não encontrado')
-    }
-    res.send(produtoEncontrado)
-})
-  
 app.post('/cadastro',async (req,res) =>{
     const usuarioCriado = await modelodeUsuario.create({email: req.body.email, senha: req.body.senha, endereço: req.body.endereço, cidade: req.body.cidade, cep: req.body.cep, estado: req.body.estado, telefone: req.body.telefone, admin: req.body.admin})
     res.send(usuarioCriado)
 })
 
-app.post('/admin',async (req,res) =>{
+
+app.post('/postProdutos/admin',async (req,res) =>{
     const usuarioEncontrado = await modelodeUsuario.findOne({email: req.body.email, senha: req.body.senha})
     if (usuarioEncontrado.eadmin === null) {
         return res.send('Usuário não permitido (tem que ser admin)')
     }
-    const produtoCriado = await modelodeUsuario.create({nomedoProduto: req.body.nomedoProduto, valordoProduto: req.body.valordoProduto})
+    const produtoCriado = await modelodeProduto.create({nomedoProduto: req.body.nomedoProduto, valordoProduto: req.body.valordoProduto})
     res.send(produtoCriado)
 })
+
+app.post('/getProdutos/', async (req,res)=>{
+    const produtoEncontrado = await modelodeProduto.findOne({nomedoProduto: req.body.nomedoProduto, valordoProduto: req.body.valordoProduto})
+    if(!produtoEncontrado){
+        return res.send('Produto não encontrado')
+    }
+    res.send(produtoEncontrado)
+})
+
+app.put('/putProdutos', async (req,res)=>{
+    const produtoAtualizado = await modelodeProduto.findOneAndUpdate(
+        {nomedoProduto: req.body.nomedoProduto, valordoProduto: req.body.valordoProduto},
+        {nomedoProduto: req.body.novoNomedoProduto, valordoProduto: req.body.novoValordoProduto},
+        {new: true})
+    res.send(produtoAtualizado)
+})
+
+app.delete('/deleteProdutos', async (req,res)=>{
+    const produtoDeletado = await modelodeProduto.deleteOne({nomedoProduto: req.body.nomedoProduto, valordoProduto: req.body.valordoProduto})
+    res.send(produtoDeletado)
+})
+
+
 
 app.put('/put', async (req,res)=>{
     const usuarioAtualizado = await modelodeUsuario.findOneAndUpdate({email: req.body.email, senha: req.body.senha}, {email: req.body.novoemail, senha: req.body.novasenha})
